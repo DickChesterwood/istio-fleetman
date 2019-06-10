9ad120739485bf5ff4e23eae763fcb5dc4d85660
Dick Chesterwood <richard@inceptiontraining.co.uk>
2019-06-10 20:08:38 +0100

    small progress. pick up point queue not working

--- istio-fleetman-api-gateway/src/main/java/com/virtualpairprogrammers/api/FleetmanApiGateway.java
+++ istio-fleetman-api-gateway/src/main/java/com/virtualpairprogrammers/api/FleetmanApiGateway.java
@@ -2,13 +2,11 @@ package com.virtualpairprogrammers.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-@EnableCircuitBreaker
 @EnableFeignClients
 public class FleetmanApiGateway {
 	public static void main(String[] args) {
