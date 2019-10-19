e759f8708a3fad816f9796fcf32ef6593919de97
Dick Chesterwood <richard@inceptiontraining.co.uk>
2019-10-19 17:49:45 +0100

    case #24 trying to remove photo service

--- istio-fleetman-staff-service/src/main/java/com/virtualpairprogrammers/staffmanagement/services/StaffService.java
+++ istio-fleetman-staff-service/src/main/java/com/virtualpairprogrammers/staffmanagement/services/StaffService.java
@@ -4,7 +4,6 @@ import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.virtualpairprogrammers.staffmanagement.domain.StaffRecord;
@@ -12,8 +11,6 @@ import com.virtualpairprogrammers.staffmanagement.domain.StaffRecord;
 @Service
 public class StaffService
 {
-	@Autowired
-	private ExternalPhotoService externalPhotoService;
 	private Map<String, String> drivers = Stream.of(new String[][] {
 		{"City Truck", "Pam Parry"},
 	    {"Huddersfield Truck A", "Duke T. Dog"},
@@ -21,10 +18,18 @@ public class StaffService
 	    {"London Riverside", "Herman Boyce"},
 	    {"Village Truck", "June Snell"}}).collect(Collectors.toMap(data -> data[0], data -> data[1]));
 	
+	private Map<String, String> photos = Stream.of(new String[][] {
+		{"Pam Parry","https://rac-istio-course-images.s3.amazonaws.com/1.jpg"},
+	    {"Duke T. Dog","https://rac-istio-course-images.s3.amazonaws.com/2.jpg"},
+	    {"Denzil Tulser","https://rac-istio-course-images.s3.amazonaws.com/3.jpg"},
+	    {"Herman Boyce","https://rac-istio-course-images.s3.amazonaws.com/4.jpg"},
+	    {"June Snell","https://rac-istio-course-images.s3.amazonaws.com/5.jpg"}}).collect(Collectors.toMap(data -> data[0], data -> data[1]));
+	
+	
 	public StaffRecord getDriverDetailsFor(String vehicleName) 
 	{
 		String driverName = drivers.get(vehicleName);
-		String staffPhoto = externalPhotoService.getStaffPhotoFor(driverName);
+		String staffPhoto = photos.get(driverName);
 		return new StaffRecord(driverName, staffPhoto);
 	}
 
