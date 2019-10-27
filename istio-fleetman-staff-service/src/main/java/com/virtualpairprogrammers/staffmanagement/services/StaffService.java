3f7fd4d4037d6c5a336383b4dc82de7055e0c0af
Dick Chesterwood <richard@inceptiontraining.co.uk>
2019-10-27 18:10:51 +0000

    case #27

--- istio-fleetman-staff-service/src/main/java/com/virtualpairprogrammers/staffmanagement/services/StaffService.java
+++ istio-fleetman-staff-service/src/main/java/com/virtualpairprogrammers/staffmanagement/services/StaffService.java
@@ -18,18 +18,12 @@ public class StaffService
 	    {"London Riverside", "Herman Boyce"},
 	    {"Village Truck", "June Snell"}}).collect(Collectors.toMap(data -> data[0], data -> data[1]));
 	
-	private Map<String, String> photos = Stream.of(new String[][] {
-		{"Pam Parry","https://rac-istio-course-images.s3.amazonaws.com/1.jpg"},
-	    {"Duke T. Dog","https://rac-istio-course-images.s3.amazonaws.com/2.jpg"},
-	    {"Denzil Tulser","https://rac-istio-course-images.s3.amazonaws.com/3.jpg"},
-	    {"Herman Boyce","https://rac-istio-course-images.s3.amazonaws.com/4.jpg"},
-	    {"June Snell","https://rac-istio-course-images.s3.amazonaws.com/5.jpg"}}).collect(Collectors.toMap(data -> data[0], data -> data[1]));
-	
+	private static final String PLACEHOLDER="https://rac-istio-course-images.s3.amazonaws.com/placeholder.png";
 	
 	public StaffRecord getDriverDetailsFor(String vehicleName) 
 	{
 		String driverName = drivers.get(vehicleName);
-		String staffPhoto = photos.get(driverName);
+		String staffPhoto = PLACEHOLDER;
 		return new StaffRecord(driverName, staffPhoto);
 	}
 
